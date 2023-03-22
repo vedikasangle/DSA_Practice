7873a5bf641e7c3b70dea7a85f6d6402bbe1e769
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-03-22 22:18:55 +0530

    Simple input

---
+++ simpleinput.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+     int n ;
+    cin>>n ; //5
+
+    int arr1[1000];
+
+    for(int p = 0 ; p<=n-1 ; p++){
+        cin>>arr1[p];
+    }
+    
+    int sum = 0 ; 
+    
+
+
+    
+    
+    
+    return 0;  
+}
